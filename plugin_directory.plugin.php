36782c4fd687babff86d9ca512b460c1d042972a
michaeltwofish <USER@DOMAIN>
2008-08-23 05:54:23 +0000

    Add SQLite schema.
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/plugin_directory/trunk@808 f4a90942-1786-4aa1-b092-57c31935ddc3

--- plugin_directory.plugin.php
+++ plugin_directory.plugin.php
@@ -121,6 +121,21 @@ require 'pluginrepo.php';
 						UNIQUE KEY id (id)
 						);";
 						break;
+					case 'sqlite':
+						$schema= "CREATE TABLE " . DB::table('plugin_versions') . " (
+						id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+						post_id INTEGER UNSIGNED NOT NULL,
+						url VARCHAR(255) NOT NULL,
+						version VARCHAR(255) NOT NULL,
+						md5 VARCHAR(255) NOT NULL,
+						status VARCHAR(255) NOT NULL,
+						habari_version VARCHAR(255) NOT NULL,
+						requires VARCHAR(255) NOT NULL,
+						provides VARCHAR(255) NOT NULL,
+						recomends VARCHAR(255) NOT NULL,
+						description TEXT
+						);";
+						break;
 				}
 				
 				if ( DB::dbdelta( $schema ) ) {
