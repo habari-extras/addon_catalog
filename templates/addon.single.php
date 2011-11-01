c3b54722a29bd056ea9ecde8fe663ba83cae7961
mikelietz <codger+git@gmail.com>
2011-11-01 20:25:44 +0000

    Whitespace cleanup prior to putting some actual usable stuff in here.

--- templates/addon.single.php
+++ templates/addon.single.php
@@ -1,8 +1,7 @@
-<?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
-<?php
+<?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); }
 
 	$theme->display('header');
-	
+
 	// figure out the tags we should display
 	if ( count( $post->tags ) > 0 ) {
 		$tags = _t( 'Tagged %s', array( Format::tag_and_list( $post->tags, ', ', ', ' ) ) );
@@ -12,16 +11,15 @@
 	}
 
 	?>
-	
 		<div id="post-<?php echo $post->id; ?>" class="<?php echo $theme->post_class( $post ); ?>">
 			<h2 class="entry-title">
 				<a href="<?php echo $post->permalink; ?>" title="<?php echo Utils::htmlspecialchars( _t( 'Permalink to %s', array( $post->title ), 'plugin_directory' ) ); ?>"><?php echo $post->title_out; ?></a>
 			</h2>
-			
+
 			<div class="entry-meta">
 				<?php echo _t( 'Posted on %1$s at %2$s by %3$s', array( $post->pubdate->date, $post->pubdate->time, $post->author->displayname ), 'plugin_directory' ); ?>
 			</div>
-			
+
 			<div class="entry-summary">
 				<p>
 					<?php echo $post->content; ?>
@@ -30,19 +28,18 @@
 			</div>
 			<hr>
 			<div class="entry-utility">
-				<?php 
-				
+				<?php
 					if ( $tags != null ) {
 						?>
 							<span class="tags"><?php echo $tags; ?></span>
 							<span class=meta-sep"> | </span>
 						<?php
 					}
-				
+
 				?>
 				<span class="comments-link"><?php $theme->comments_link( $post ); ?></span>
-				<?php 
-				
+				<?php
+
 					if ( ACL::access_check( $post->get_access(), 'edit' ) ) {
 						?>
 							<span class="meta-sep"> | </span>
@@ -51,14 +48,13 @@
 							</span>
 						<?php
 					}
-				
+
 				?>
 			</div>
-		
 		</div>
-	
+
 	<?php
-	
+
 	$theme->display('footer');
 
 ?>
