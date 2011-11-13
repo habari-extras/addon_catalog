b483a715df78f4cad36b276433ce6da9e02e84bf
mikelietz <codger+git@gmail.com>
2011-11-13 16:57:08 -0500

    Build a breadcrumb with a dirty, dirty hack. Closes #13.

--- templates/directory_header.php
+++ templates/directory_header.php
@@ -1,3 +1,15 @@
 <div id="addons-header">
-<a href="<?php URL::out( array( "display_addon_basepath", "display_basepath" ) ); ?>">Addons</a>
+<small><a href="<?php URL::out( array( "display_addon_basepath", "display_basepath" ) ); ?>"><?php Options::out( 'plugin_directory__basepath', 'explore' ); ?></a>
+<?php
+	switch (true) {
+	case ( $request->display_addons ):?>
+/ <a href="<?php URL::out( "display_addons", array( 'addon' => $posts[0]->info->type . "s" ) ); ?>"><?php echo $posts[0]->info->type . "s"; ?></a>
+<?php		break;
+ 	case ( $request->display_addon ):?>
+/ <a href="<?php URL::out( "display_addons", array( 'addon' => $post->info->type . "s" ) ); ?>"><?php echo $post->info->type . "s"; ?></a>
+/ <a href="<?php echo $post->permalink; ?>"><?php echo $post->title; ?></a>
+<?php		break;
+} ?>
+</small>
 </div>
+<hr>
