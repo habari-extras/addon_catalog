265b4f7fc3e683b8380ee065ce18eae0f5298bc0
mikelietz <codger+git@gmail.com>
2012-12-18 03:33:58 +0000

    Offload default content creation to separate file - it will potentially get much larger.

--- addons_directory.plugin.php
+++ addons_directory.plugin.php
@@ -77,49 +77,7 @@
 			// @TODO: notification/log of some sort?
 
 			// create the default content
-			$this->create_default_content();
-		}
-
-		private function create_default_content ( ) {
-
-			$habari_addon = Posts::get( array( 'content_type' => 'addon', 'slug' => 'habari' ) );
-
-			if ( count( $habari_addon ) == 0 ) {
-				$habari = Post::create( array(
-					'content_type' => Post::type( 'addon' ),
-					'title' => 'Habari',
-					'content' => file_get_contents( dirname( __FILE__ ) . '/addon.habari.txt' ),
-					'status' => Post::status('published'),
-					'tags' => array( 'habari' ),
-					'pubdate' => HabariDateTime::date_create(),
-					'user_id' => User::identify()->id,
-					'slug' => 'habari',
-				) );
-
-				$habari->info->guid = '7a0313be-d8e3-11db-8314-0800200c9a66';
-				$habari->info->url = 'http://habariproject.org';
-				$habari->info->description = 'Habari is next-generation blogging.';
-				$habari->info->authors = array( array( 'name' => 'The Habari Community', 'url' => 'http://habariproject.org' ) );
-				$habari->info->licenses = array( array( 'name' => 'Apache License, Version 2.0', 'url' => 'http://www.apache.org/licenses/LICENSE-2.0' ) );
-				$habari->info->type = 'core';
-				$habari->info->commit();
-
-				$versions = array(
-					'0.8' => array(
-						'version' => '0.8',
-						'description' => 'Habari release 0.8',
-						'info_url' => 'http://wiki.habariproject.org/en/Release_0.8',
-						'url' => 'http://habariproject.org/dist/habari-0.8.zip',
-						'habari_version' => '0.8',
-						'severity' => 'feature',
-						'requires' => '',
-						'provides' => '',
-						'recommends' => '',
-						'release' => HabariDateTime::date_create( '2011-12-13' )->sql,
-					),
-				);
-				$this->save_versions( $habari, $versions );
-			}
+			include( 'create_core_addons.php' );
 		}
 
 		public function action_plugin_deactivation ( $file ) {
---
+++ create_core_addons.php
@@ -0,0 +1,76 @@
+<?php
+$habari_addon = Posts::get( array( 'content_type' => 'addon', 'slug' => 'habari' ) );
+
+if ( count( $habari_addon ) == 0 ) {
+	$habari = Post::create( array(
+		'content_type' => Post::type( 'addon' ),
+		'title' => 'Habari',
+		'content' => file_get_contents( dirname( __FILE__ ) . '/addon.habari.txt' ),
+		'status' => Post::status('published'),
+		'tags' => array( 'habari' ),
+		'pubdate' => HabariDateTime::date_create(),
+		'user_id' => User::identify()->id,
+		'slug' => 'habari',
+	) );
+	$habari->info->guid = '7a0313be-d8e3-11db-8314-0800200c9a66';
+	$habari->info->url = 'http://habariproject.org';
+	$habari->info->description = 'Habari is next-generation blogging.';
+	$habari->info->authors = array( array( 'name' => 'The Habari Community', 'url' => 'http://habariproject.org' ) );
+	$habari->info->licenses = array( array( 'name' => 'Apache License, Version 2.0', 'url' => 'http://www.apache.org/licenses/LICENSE-2.0' ) );
+	$habari->info->type = 'core';
+	$habari->info->commit();
+
+	$versions = array(
+		'0.9' => array(
+			'version' => '0.9',
+			'description' => 'Habari release 0.9',
+			'info_url' => 'http://wiki.habariproject.org/en/Release_0.9',
+			'url' => 'http://habariproject.org/dist/habari-0.9.zip',
+			'habari_version' => '0.9',
+			'severity' => 'feature',
+			'requires' => '',
+			'provides' => '',
+			'recommends' => '',
+			'release' => HabariDateTime::date_create( '2012-11-20' )->sql,
+		),
+	);
+
+	$this->save_versions( $habari, $versions );
+
+	$wazi = Post::create( array(
+		'content_type' => Post::type( 'addon' ),
+		'title' => 'Wazi',
+		'content' => file_get_contents( Site::$config_path . '/system/themes/wazi/README' ),
+		'status' => Post::status('published'),
+		'tags' => array( 'habari', 'theme' ),
+		'pubdate' => HabariDateTime::date_create(),
+		'user_id' => User::identify()->id,
+		'slug' => 'wazi',
+	) );
+	$wazi->info->guid = '';
+	$wazi->info->url = 'http://habariproject.org';
+	$wazi->info->description = 'Responsive theme included with Habari.';
+	$wazi->info->authors = array( array( 'name' => 'The Habari Community', 'url' => 'http://habariproject.org' ) );
+	$wazi->info->licenses = array( array( 'name' => 'Apache License, Version 2.0', 'url' => 'http://www.apache.org/licenses/LICENSE-2.0' ) );
+	$wazi->info->type = 'theme';
+	$wazi->info->commit();
+
+	$versions = array(
+		'0.9' => array(
+			'version' => '0.9',
+			'description' => 'Wazi',
+			'info_url' => 'http://wiki.habariproject.org/en/Release_0.9',
+			'url' => 'http://habariproject.org/dist/habari-0.9.zip',
+			'habari_version' => '0.9',
+			'severity' => 'feature',
+			'requires' => '',
+			'provides' => '',
+			'recommends' => '',
+			'release' => HabariDateTime::date_create( '2012-11-20' )->sql,
+		),
+	);
+
+	$this->save_versions( $wazi, $versions );
+
+}
+?>
