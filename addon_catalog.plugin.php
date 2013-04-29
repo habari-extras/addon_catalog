3403252726ed685851c91bbe5035e71de1ff18bb
Chris J. Davis <chrisjdavis@contrib.habariproject.org>
2013-04-29 09:43:35 -0500

    Register the template

--- addon_catalog.plugin.php
+++ addon_catalog.plugin.php
@@ -629,6 +629,7 @@ class AddonCatalogPlugin extends Plugin {
 		$this->add_template( 'addon.basepath', dirname(__FILE__) . '/templates/addon.basepath.php' );
 		$this->add_template( 'addon.multiple', dirname(__FILE__) . '/templates/addon.multiple.php' );
 		$this->add_template( 'addon.single', dirname(__FILE__) . '/templates/addon.single.php' );
+		$this->add_template( 'addon.tag', dirname(__FILE__) . '/templates/addon.tag.php' );
 		$this->add_template( 'directory_header', dirname(__FILE__) . '/templates/directory_header.php' );
 
 		// register admin pages
